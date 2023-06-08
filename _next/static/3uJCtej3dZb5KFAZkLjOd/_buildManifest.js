b1c40eec4023fd152daac7f90fc1d7aa67360ea9
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-06-08 18:03:30 +0900

    Updates

--- _next/static/3uJCtej3dZb5KFAZkLjOd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/841-6a34d139adaccb51.js","static/chunks/pages/index-4a0b007f5e00142d.js"],"/_error":["static/chunks/pages/_error-bd1da5a6907513b5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/6XNev8nMyDvnV3ULLCaI1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/841-6a34d139adaccb51.js","static/chunks/pages/index-6bd36c940e0b7196.js"],"/_error":["static/chunks/pages/_error-bd1da5a6907513b5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
