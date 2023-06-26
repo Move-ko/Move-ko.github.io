2d361ef8f8124c409412729da4fe7e42ee8459b8
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-06-26 16:23:19 +0900

    add

--- src/components/study/7_move.js
+++ src/components/study/7_move.js
@@ -82,9 +82,13 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            Move에는 불변 & 및 가변 &mut의 두 가지 유형의 참조가 있습니다.
-            변경할 수 없는 참조는 읽기 전용이며 기본 값(또는 해당 필드)을 수정할
-            수 없습니다. 변경 가능한 참조는 해당 참조를 통한 쓰기를 통한 수정을
+            Move에는 <span style={{ color: "purple" }}>불변 &</span>및{" "}
+            <span style={{ color: "purple" }}>가변 &mut</span>의 두 가지 유형의
+            참조가 있습니다. 변경할 수 없는 참조는{" "}
+            <span style={{ color: "purple" }}> 읽기 전용</span>
+            이며 기본 값(또는 해당 필드)을 수정할 수 없습니다. 변경 가능한
+            참조는 해당 참조를 통한{" "}
+            <span style={{ color: "purple" }}>쓰기를 통한 수정</span>을
             허용합니다. Move의 유형 시스템은 참조 오류를 방지하는 소유권 규율을
             적용합니다. 참조 규칙에 대한 자세한 내용은 구조체 및 리소스를
             참조하세요.
@@ -92,7 +96,7 @@ const study_1 = ({ children }) => {
         </Box>
       </Grid>
       <Grid xs={12}>
-        <Box sx={{ width: "100%" }}>
+        <Box sx={{ width: "100%", marginTop: "30px" }}>
           <Typography variant="h4" gutterBottom>
             참조 연산자
           </Typography>
@@ -103,7 +107,12 @@ const study_1 = ({ children }) => {
           <Typography variant="body1" gutterBottom>
             Move는 참조를 만들고 확장할 뿐만 아니라 변경 가능한 참조를 변경
             불가능한 참조로 변환하기 위한 연산자를 제공합니다. 여기와 다른
-            곳에서는 "표현식 e에는 T 유형이 있음"에 대해 표기법 e: T를
+            곳에서는{" "}
+            <span style={{ color: "purple" }}>
+              {" "}
+              "표현식 e에는 T 유형이 있음"
+            </span>
+            에 대해 표기법 <span style={{ color: "purple" }}> e: T</span>를
             사용합니다.
           </Typography>
         </Box>
@@ -135,14 +144,19 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            &e.f 및 &mut e.f 연산자는 구조체에 대한 새 참조를 만들거나 기존
-            참조를 확장하는 데 모두 사용할 수 있습니다.
+            <span style={{ color: "purple" }}> &e.f</span>및{" "}
+            <span style={{ color: "purple" }}> &mut e.f </span>
+            연산자는 구조체에 대한 새 참조를 만들거나 기존 참조를 확장하는 데
+            모두 사용할 수 있습니다.
           </Typography>
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
-      <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+      <Grid xs={12} md={4} sx={{ marginTop: "30px" }}>
+        <img
+          src={"/img/7_1.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
@@ -154,24 +168,31 @@ const study_1 = ({ children }) => {
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
-      <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+      <Grid xs={12} md={4} sx={{ marginTop: "30px" }}>
+        <img
+          src={"/img/7_2.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            마지막으로 참조에 대한 참조는 허용되지 않습니다.
+            마지막으로 참조에 대한 참조는{" "}
+            <span style={{ color: "purple" }}>허용되지 않습니다.</span>
           </Typography>
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
-      <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+      <Grid xs={12} md={4} sx={{ marginTop: "30px" }}>
+        <img
+          src={"/img/7_3.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12}>
-        <Box sx={{ width: "100%" }}>
+        <Box sx={{ width: "100%", marginTop: "30px" }}>
           <Typography variant="h4" gutterBottom>
             참고 문헌을 통한 읽기 및 쓰기{" "}
           </Typography>
@@ -181,10 +202,13 @@ const study_1 = ({ children }) => {
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
             변경 가능한 참조와 변경 불가능한 참조를 모두 읽어 참조된 값의
-            복사본을 생성할 수 있습니다. 변경 가능한 참조만 작성할 수 있습니다.
-            쓰기 *x = v는 이전에 x에 저장된 값을 버리고 v로 업데이트합니다. 두
-            작업 모두 C와 유사한 * 구문을 사용합니다. 그러나 읽기는 표현식인
-            반면 쓰기는 등호의 왼쪽에서 발생해야 하는 변형입니다.
+            <span style={{ color: "purple" }}>복사본</span>을 생성할 수
+            있습니다. 변경 가능한 참조만 작성할 수 있습니다. 쓰기{" "}
+            <span style={{ color: "purple" }}> *x = v</span>는 이전에 x에 저장된
+            값을 버리고 v로 업데이트합니다. 두 작업 모두 C와 유사한{" "}
+            <span style={{ color: "purple" }}>* 구문</span>을 사용합니다. 그러나
+            읽기는 표현식인 반면 쓰기는 등호의 왼쪽에서 발생해야 하는
+            변형입니다.
           </Typography>
         </Box>
       </Grid>
@@ -215,29 +239,41 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            참조를 읽으려면 기본 유형에 복사 기능이 있어야 합니다. 참조를 읽으면
-            값의 새 복사본이 생성되기 때문입니다. 이 규칙은 리소스 값 복사를
-            방지합니다.
+            참조를 읽으려면 기본 유형에{" "}
+            <span style={{ color: "purple" }}>복사</span>
+            기능이 있어야 합니다. 참조를 읽으면 값의 새 복사본이 생성되기
+            때문입니다.{" "}
+            <span style={{ color: "purple" }}>
+              {" "}
+              이 규칙은 리소스 값 복사를 방지합니다.
+            </span>
           </Typography>
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/7_4.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
             이중: 참조를 작성하려면 기본 유형에 삭제 기능이 있어야 합니다.
-            참조에 작성하면 이전 값이 삭제(또는 "삭제")되기 때문입니다. 이
-            규칙은 리소스 값의 파괴를 방지합니다.
+            참조에 작성하면 이전 값이 삭제 (
+            <span style={{ color: "purple" }}>또는 "삭제"</span>)되기
+            때문입니다. 이 규칙은 리소스 값의 파괴를 방지합니다.
           </Typography>
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/7_5.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12}>
@@ -257,7 +293,10 @@ const study_1 = ({ children }) => {
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/7_6.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
@@ -270,7 +309,10 @@ const study_1 = ({ children }) => {
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/7_7.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12}>
@@ -283,17 +325,22 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            이 동결 추론을 통해 Move 유형 검사기는 &mut T를 &T의 하위 유형으로
-            볼 수 있습니다. 위에 표시된 것처럼 이것은 &T 값이 사용되는 모든
-            표현식에 대해 &mut T 값도 사용할 수 있음을 의미합니다. 이 용어는
-            &T가 제공된 곳에 &mut T가 필요했음을 간결하게 나타내기 위해 오류
-            메시지에 사용됩니다. 예를 들어
+            이 동결 추론을 통해 Move 유형 검사기는{" "}
+            <span style={{ color: "purple" }}>&mut T</span>를{" "}
+            <span style={{ color: "purple" }}>&T</span>의 하위 유형으로 볼 수
+            있습니다. 위에 표시된 것처럼 이것은 &T 값이 사용되는 모든 표현식에
+            대해 &mut T 값도 사용할 수 있음을 의미합니다. 이 용어는 &T가 제공된
+            곳에 &mut T가 필요했음을 간결하게 나타내기 위해 오류 메시지에
+            사용됩니다. 예를 들어
           </Typography>
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/7_8.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
@@ -305,13 +352,17 @@ const study_1 = ({ children }) => {
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/7_9.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            현재 하위 유형이 있는 유일한 다른 유형은 튜플입니다.
+            현재 하위 유형이 있는 유일한 다른 유형은{" "}
+            <span style={{ color: "purple" }}>튜플</span>입니다.
           </Typography>
         </Box>
       </Grid>
@@ -332,7 +383,10 @@ const study_1 = ({ children }) => {
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/7_10.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
@@ -355,23 +409,36 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            참조와 튜플은 구조체의 필드 값으로 저장할 수 없는 유일한 유형이며,
-            이는 전역 저장소에 존재할 수 없음을 의미하기도 합니다. 프로그램 실행
-            중에 생성된 모든 참조는 Move 프로그램이 종료될 때 삭제됩니다. 그들은
-            완전히 일시적입니다. 이 불변성은 저장 기능이 없는 유형의 값에도
-            적용되지만 참조와 튜플은 애초에 구조체에서 허용되지 않음으로써 한
-            단계 더 나아갑니다. 이것은 Move와 Rust의 또 다른 차이점으로, 구조체
-            내부에 참조를 저장할 수 있습니다. 현재 Move는 참조를 직렬화할 수
-            없기 때문에 이를 지원할 수 없지만 모든 Move 값은 직렬화 가능해야
-            합니다. 이 요구 사항은 프로그램 실행 간에 값을 유지하기 위해 값을
-            직렬화해야 하는 Move의 영구 전역 저장소에서 비롯됩니다. 구조체는
-            전역 저장소에 쓸 수 있으므로 직렬화 가능해야 합니다. 참조를 구조체에
-            저장하고 해당 구조체가 전역 저장소에 존재하는 것을 금지하는 더
-            멋지고 표현력이 뛰어난 유형 시스템을 상상할 수 있습니다. 저장 기능이
-            없는 구조체 내부의 참조를 허용할 수도 있지만 문제를 완전히
-            해결하지는 못합니다. Move에는 정적 참조 안전성을 추적하는 상당히
-            복잡한 시스템이 있으며 유형 시스템의 이러한 측면도 확장해야 합니다.
-            구조체 내부에 참조 저장을 지원합니다. 요컨대, Move의 유형
+            <span style={{ color: "purple" }}>참조</span> 와{" "}
+            <span style={{ color: "purple" }}>튜플</span>은 구조체의 필드 값으로
+            저장할 수 없는 <span style={{ color: "purple" }}>유일한 유형</span>
+            이며, 이는{" "}
+            <span style={{ color: "purple" }}>
+              전역 저장소에 존재할 수 없음을 의미
+            </span>
+            하기도 합니다. 프로그램 실행 중에 생성된 모든 참조는{" "}
+            <span style={{ color: "purple" }}>
+              Move 프로그램이 종료될 때 삭제
+            </span>
+            됩니다. 그들은 완전히{" "}
+            <span style={{ color: "purple" }}> 일시적</span> 입니다. 이 불변성은
+            저장 기능이 없는 유형의 값에도 적용되지만 참조와 튜플은 애초에
+            구조체에서 허용되지 않음으로써 한 단계 더 나아갑니다. 이것은 Move와
+            Rust의 또 다른 차이점으로, 구조체 내부에 참조를 저장할 수 있습니다.
+            현재 Move는 참조를 직렬화할 수 없기 때문에 이를 지원할 수 없지만
+            모든 Move 값은 직렬화 가능해야 합니다. 이 요구 사항은 프로그램 실행
+            간에 값을 유지하기 위해 값을 직렬화해야 하는 Move의 영구 전역
+            저장소에서 비롯됩니다.{" "}
+            <span style={{ color: "purple" }}>
+              {" "}
+              구조체는 전역 저장소에 쓸 수 있으므로 직렬화 가능해야 합니다.
+            </span>
+            참조를 구조체에 저장하고 해당 구조체가 전역 저장소에 존재하는 것을
+            금지하는 더 멋지고 표현력이 뛰어난 유형 시스템을 상상할 수 있습니다.
+            저장 기능이 없는 구조체 내부의 참조를 허용할 수도 있지만 문제를
+            완전히 해결하지는 못합니다. Move에는 정적 참조 안전성을 추적하는
+            상당히 복잡한 시스템이 있으며 유형 시스템의 이러한 측면도 확장해야
+            합니다. 구조체 내부에 참조 저장을 지원합니다. 요컨대, Move의 유형
             시스템(특히 참조 안전성에 관한 측면)은 저장된 참조를 지원하도록
             확장되어야 합니다. 그러나 그것은 언어가 발전함에 따라 우리가
             주시하고 있는 것입니다.
--- src/components/study/8_move.js
+++ src/components/study/8_move.js
@@ -16,7 +16,25 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            글
+            Move는 튜플을 일급 값으로 사용하는 다른 언어에서 온 것으로 예상할 수
+            있으므로 튜플을 완전히 지원하지 않습니다. 그러나 여러 반환 값을
+            지원하기 위해 Move에는 튜플과 같은 표현식이 있습니다. 이러한 식은
+            런타임에 구체적인 값을 생성하지 않으며(바이트코드에 튜플이 없음)
+            결과적으로 매우 제한적입니다. 식에만 나타날 수 있습니다(일반적으로
+            함수의 반환 위치). 지역 변수에 바인딩할 수 없습니다. 구조체에 저장할
+            수 없습니다. 제네릭을 인스턴스화하는 데 튜플 유형을 사용할 수
+            없습니다. 마찬가지로 unit()은 표현식 기반이 되기 위해 Move 소스
+            언어에서 만든 유형입니다. 단위 값()은 런타임 값을 생성하지 않습니다.
+            unit()을 빈 튜플로 간주할 수 있으며 튜플에 적용되는 모든 제한 사항이
+            유닛에도 적용됩니다. 이러한 제한 사항을 감안할 때 언어에 튜플이 있는
+            것이 이상하게 느껴질 수 있습니다. 그러나 다른 언어에서 튜플의 가장
+            일반적인 사용 사례 중 하나는 함수가 여러 값을 반환할 수 있도록 하는
+            함수입니다. 일부 언어는 사용자가 여러 반환 값을 포함하는 구조체를
+            작성하도록 강제하여 이 문제를 해결합니다. 그러나 Move에서는 구조체
+            내부에 참조를 넣을 수 없습니다. 여러 반환 값을 지원하려면 Move가
+            필요했습니다. 이러한 여러 반환 값은 모두 바이트 코드 수준에서 스택에
+            푸시됩니다. 소스 수준에서 이러한 여러 반환 값은 튜플을 사용하여
+            표시됩니다.
           </Typography>
         </Box>
       </Grid>
@@ -30,7 +48,7 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            글
+            튜플은 괄호 안에 쉼표로 구분된 표현식 목록으로 생성됩니다.
           </Typography>
         </Box>
       </Grid>
@@ -42,13 +60,19 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            글
+            (e)에는 (e): (t) 유형이 없습니다. 즉, 하나의 요소가 있는 튜플이
+            없습니다. 괄호 안에 하나의 요소만 있는 경우 괄호는 명확성을 위해서만
+            사용되며 다른 특별한 의미는 없습니다. 때로는 요소가 두 개인 튜플을
+            "쌍"이라고 하고 요소가 세 개인 튜플을 "트리플"이라고 합니다.
           </Typography>
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/8_1.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12}>
@@ -61,7 +85,7 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            글
+            현재 튜플에서 수행할 수 있는 유일한 작업은 구조 분해입니다.
           </Typography>
         </Box>
       </Grid>
@@ -75,19 +99,22 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            글
+            모든 크기의 튜플에 대해 let 바인딩이나 할당에서 해체될 수 있습니다.
           </Typography>
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/8_2.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            글
+            자세한 내용은 변수 이동을 참조하십시오.
           </Typography>
         </Box>
       </Grid>
@@ -101,13 +128,18 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            글
+            참조와 함께 튜플은 Move에서 하위 유형이 있는 유일한 유형입니다.
+            튜플은 (공변량 방식으로) 참조가 있는 하위 유형이라는 의미에서만 하위
+            유형이 있습니다.
           </Typography>
         </Box>
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12} md={4} sx={{ marginTop: "0px" }}>
-        사진
+        <img
+          src={"/img/8_3.png"}
+          style={{ width: "100%", borderRadius: "10px", marginTop: "10px" }}
+        />
       </Grid>
       <Grid xs={0} md={4}></Grid>
       <Grid xs={12}>
@@ -120,7 +152,10 @@ const study_1 = ({ children }) => {
       <Grid xs={12} md={12} sx={{ marginTop: "30px" }}>
         <Box sx={{ width: "100%", textAlign: "left" }}>
           <Typography variant="body1" gutterBottom>
-            글
+            위에서 언급했듯이 튜플 값은 런타임에 실제로 존재하지 않습니다.
+            그리고 현재 이 때문에 로컬 변수에 저장할 수 없습니다(그러나 이
+            기능이 곧 제공될 가능성이 높습니다). 따라서 튜플을 복사하려면 먼저
+            지역 변수에 넣어야 하므로 튜플은 현재에만 이동할 수 있습니다.
           </Typography>
         </Box>
       </Grid>
