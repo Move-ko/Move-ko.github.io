4ec5b921fbf6c90f984bfe1b857cfcf427200e85
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-06-26 17:12:59 +0900

    Updates

--- _next/static/nhSk3WHEL7VGtO7Ds0YYd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/787-f0c43724a8311abd.js","static/chunks/pages/index-1e3c99e9f2fe63e9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
