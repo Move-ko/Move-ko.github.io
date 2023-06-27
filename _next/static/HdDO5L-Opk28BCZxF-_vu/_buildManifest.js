b459d83e44c90e4143f18d81d9690538baa4644d
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-06-27 16:33:20 +0900

    Updates

--- _next/static/HdDO5L-Opk28BCZxF-_vu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/787-f0c43724a8311abd.js","static/chunks/pages/index-fb939bdc88c5cbd1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/uZS6b4fp5CdyhUG51Lbf9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/787-f0c43724a8311abd.js","static/chunks/pages/index-13c8261b8d081205.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
