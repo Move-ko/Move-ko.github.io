033cddf717d7f8fcd20bb6a2257742a09adfc37a
kyunghyunHan <ekfqlwcjswl@naver.com>
2023-08-11 18:26:01 +0900

    refac

--- Move/3/3_2.move
+++ Move/3/3_2.move
@@ -1,7 +1,14 @@
 module example::test {
-    fun bool1(){
+    fun boolean(){
+      /*
+           * bool은 세가지 논리 연산을 지원합니다.      
+           &&	단락 논리 및	  p && q에 해당합니다       if(p)q else false
+           ||	단락 논리 또는	  p || q 에 해당합니다     if(p) true else q
+            !	논리적 부정	    !p에 해당합니다 if(q)     false else true
+      */
 
-
+      //bool 값은 Move의 여러 제어 흐름 구조에서 사용됩니다:
+      //* if문
         if bool_표현식 {
        // bool_표현식이 true일 경우 실행되는 코드 블록
        } else {
@@ -9,27 +16,25 @@ module example::test {
        }
 
 
-
-
-       }
-
-      fun bool2(){
-
-
-      while bool_표현식 {
+    //while
+        while bool_표현식 {
         // bool_표현식이 true인 동안 반복적으로 실행되는 코드 블록
       }
 
 
-    }
+    //assert:이 연산은 두 개의 인자를 받습니다: bool 타입의 조건과 u64 타입의 코드
+         assert!(bool_표현식, 오류_메시지);  
 
-
-      fun bool3(){
-
-
-       assert!(bool_표현식, 오류_메시지);  
-
-
-
-    }
+   
+          let a = true  //true
+          let b = false //false
+          let c = true && false //false
+          let d = false || true //true
+          let e = true || false && true //true
+          let f = !true //false
+          let g = !false  //true
+          let h = !true || true //true
+          let i = false || true && false //false  
+       }
+ 
 }
--- src/components/study/3_move.tsx
+++ src/components/study/3_move.tsx
@@ -1,69 +1,42 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import { styled } from "@mui/material/styles";
 import Copy from "../util/copy";
 
 const study_1 = () => {
-  const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
-    },
-    [`&.${tableCellClasses.body}`]: {
-      fontSize: 10,
-    },
-  }));
-
-  const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    "&:nth-of-type(odd)": {
-      backgroundColor: theme.palette.action.hover,
-    },
-    // hide last border
-    "&:last-child td, &:last-child th": {
-      border: 0,
-    },
-  }));
-
-  const bools = [
-    {
-      통사론: "&&",
-      설명: "단락 논리 및",
-      등가표현: "p && q에 해당합니다 if(p)q else false",
-    },
-    {
-      통사론: "||",
-      설명: "단락 논리 또는",
-      등가표현: "p || q 에 해당합니다 if(p) true else q",
-    },
-    {
-      통사론: "!",
-      설명: "논리적 부정",
-      등가표현: "!p에 해당합니다 if(q) false else true",
-    },
-  ];
   const code1 = `      
-  if bool_표현식 {
- // bool_표현식이 true일 경우 실행되는 코드 블록
- } else {
-  // bool_표현식이 false일 경우 실행되는 코드 블록
- }
+  module example::test {
+    fun boolean(){
+      /*
+           * bool은 세가지 논리 연산을 지원합니다.      
+           &&	단락 논리 및	  p && q에 해당합니다       if(p)q else false
+           ||	단락 논리 또는	  p || q 에 해당합니다     if(p) true else q
+            !	논리적 부정	    !p에 해당합니다 if(q)     false else true
+      */
 
-`;
-  const code2 = `      while bool_표현식 {
-    // bool_표현식이 true인 동안 반복적으로 실행되는 코드 블록
-  }
-`;
-  const code3 = `     assert!(bool_표현식, 오류_메시지); `;
+      //bool 값은 Move의 여러 제어 흐름 구조에서 사용됩니다:
+      //* if문
+        if bool_표현식 {
+       // bool_표현식이 true일 경우 실행되는 코드 블록
+       } else {
+        // bool_표현식이 false일 경우 실행되는 코드 블록
+       }
 
-  const code4 = `          let a = true  //true
+
+    //while
+        while bool_표현식 {
+        // bool_표현식이 true인 동안 반복적으로 실행되는 코드 블록
+      }
+
+
+    //assert:이 연산은 두 개의 인자를 받습니다: bool 타입의 조건과 u64 타입의 코드
+         assert!(bool_표현식, 오류_메시지);  
+
+   
+          let a = true  //true
           let b = false //false
           let c = true && false //false
           let d = false || true //true
@@ -72,7 +45,11 @@ const study_1 = () => {
           let g = !false  //true
           let h = !true || true //true
           let i = false || true && false //false  
+       }
+ 
+}
 `;
+
   return (
     <Grid container>
       <Grid xs={12}>
@@ -90,64 +67,7 @@ const study_1 = () => {
             표현됩니다.
           </Typography>
         </Box>
-        <Typography variant="body1" gutterBottom>
-          <span style={{ color: "purple" }}>
-            * bool은 세가지 논리 연산을 지원합니다.
-          </span>
-        </Typography>
-        <TableContainer component={Paper}>
-          <Table sx={{ width: "100%" }} aria-label="customized table">
-            <TableHead>
-              <TableRow>
-                <StyledTableCell>통사론</StyledTableCell>
-                <StyledTableCell>설명</StyledTableCell>
-                <StyledTableCell>등가표현</StyledTableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody component="div">
-              {bools.map(item => (
-                <StyledTableRow>
-                  <StyledTableCell component="div" scope="row">
-                    {item?.통사론}
-                  </StyledTableCell>
-                  <StyledTableCell>{item?.설명}</StyledTableCell>
-                  <StyledTableCell>{item?.등가표현}</StyledTableCell>
-                </StyledTableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <Box sx={{ width: "100%", marginTop: "30px" }}>
-          <Typography variant="body1" gutterBottom>
-            bool 값은 Move의 여러 제어 흐름 구조에서 사용됩니다:
-          </Typography>
-        </Box>
-        <Typography variant="body1" gutterBottom>
-          <span style={{ color: "purple" }}>* if문</span>
-        </Typography>
         <Copy code={code1} />
-        <Typography variant="body1" gutterBottom>
-          <span style={{ color: "purple" }}>* while문</span>
-        </Typography>
-        <Copy code={code2} />
-        <Typography variant="body1" gutterBottom>
-          <span style={{ color: "purple" }}>
-            3) assert:이 연산은 두 개의 인자를 받습니다: bool 타입의 조건과 u64
-            타입의 코드
-          </span>
-        </Typography>
-        <Copy code={code3} />
-        <Box sx={{ width: "100%", marginTop: "30px" }}>
-          <Typography variant="h4" gutterBottom>
-            정리
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            다른 스칼라 값들과 마찬가지로, 언어 내장의 boolean 값들은 암시적으로
-            복사 가능합니다. 즉, copy와 같은 명시적인 지시어 없이도 복사할 수
-            있습니다.
-          </Typography>
-        </Box>
-        <Copy code={code4} />
       </Grid>
     </Grid>
   );
